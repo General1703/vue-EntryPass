8f4eacd0c11b6363694c90b5b2b1a76311edd6ae
pc2 <pbantawig_210000000681@uic.edu.ph>
2023-11-28 14:19:07 +0800

    deleted welcome message from login

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -7,7 +7,7 @@ import Button from 'primevue/button';
 <template>
   <div>
     
-    <h1>Entry Pass (Welcome)</h1>
+    <h1>Entry Pass</h1>
     <p>Log in</p>
     <InputText v-model="value1" type="text" size="small" placeholder="UserName" />
     <InputText v-model="value1" type="text" size="small" placeholder="Password" />
